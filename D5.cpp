242574aa3b7aafb2a62f69f656d48c90acfab086
Elia <smallhazelnut@gmail.com>
2025-01-27 21:59:45 +0100

    D5 part 1 ok

--- D5.cpp
+++ D5.cpp
@@ -1,25 +1,50 @@
 #include "getinput.hpp"
+#include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
 
 int main() {
   std::vector<std::vector<int>> rules;
   std::vector<std::vector<int>> updates;
-
+  int sum = 0;
+  int iterator = 0;
   getPrintingInstruction("D5_input.txt", rules, updates);
-  /***
-std::cout << "Rules:" << std::endl;
-for (const auto &rule : rules) {
-  std::cout << rule[0] << "|" << rule[1] << std::endl;
-}
+  bool well_ordered = true;
+  for (const auto &update : updates) {
+    well_ordered = true;
+    iterator = 0;
+    for (int page : update) {
+      for (int i = 0; i < rules.size(); i++) {
+        if (rules[i][0] == page) {
+          for (int j = 0; j < iterator; j++) {
+            if (rules[i][1] == update[j]) {
+              well_ordered = false;
+              break;
+            }
+          }
+        }
+      }
 
-std::cout << "Updates:" << std::endl;
-for (const auto &update : updates) {
-  for (int num : update) {
-    std::cout << num << " ";
+      iterator++;
+    }
+    if (well_ordered && !update.empty()) {
+      int middle_index = update.size() / 2;
+      sum += update[middle_index];
+    }
   }
-  std::cout << std::endl;
-} ***/
 
+  /** std::cout << "Rules:" << std::endl;
+  for (const auto &rule : rules) {
+    std::cout << rule[0] << "|" << rule[1] << std::endl;
+  }
+
+  std::cout << "Updates:" << std::endl;
+  for (const auto &update : updates) {
+    for (int num : update) {
+      std::cout << num << " ";
+    }
+  }
+  std::cout << std::endl;**/
+  std::cout << sum << std::endl;
   return 0;
 }
