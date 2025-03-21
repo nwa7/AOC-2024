3bdb4190f6b20eb3cb7cc5ec09fb021e6b05c0b2
Elia <smallhazelnut@gmail.com>
2025-03-21 17:33:23 +0100

    day 9 started

---
+++ D9.cpp
@@ -0,0 +1,61 @@
+#include "getinput.hpp"
+#include "sortalgo.hpp"
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+int get_blocks(std::vector<int> &lines, std::vector<int> &blocks);
+int compact_blocks(std::vector<int> &blocks);
+
+int main() {
+  std::vector<int> lines;
+  std::vector<int> blocks;
+  std::string path = "D9_input.txt"; // File path
+  getLines(path, lines);
+  get_blocks(lines, blocks);
+  // compact_blocks(blocks);
+
+  return 0;
+}
+
+int get_blocks(std::vector<int> &lines, std::vector<int> &blocks) {
+  int size = lines.size();
+  std::cout << size << std::endl;
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < lines[2 * i]; j++) {
+      blocks.push_back(i);
+    }
+    for (int k = 0; k < lines[2 * i + 1]; k++) {
+      blocks.push_back(-1);
+    };
+  };
+  for (int l = 0; l < 50; l++) {
+    if (blocks[l] == -1) {
+      std::cout << ".";
+    } else {
+      std::cout << blocks[l];
+    }
+  }
+  return 0;
+};
+
+int compact_blocks(std::vector<int> &blocks) {
+  int block_size = blocks.size();
+  for (int i = 0; i < block_size; i++) {
+    if (blocks[i] == -1) {
+      int replacement = blocks[block_size];
+      int count = 1;
+      while (replacement == -1 || block_size - count != i) {
+        count++;
+        replacement = blocks[block_size - count];
+      }
+      std::swap(blocks[i], blocks[block_size - count]);
+    }
+    for (int j = 0; j < block_size; j++) {
+      std::cout << blocks[i];
+    };
+    std::cout << " " << std::endl;
+  };
+  return 0;
+};
